28916ea64c7dbaef645ec388f7a1a536791d6bf0
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-02-12 17:58:13 +0900

    [Silver III] Title: 개미, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input().strip())
+
+x = (p + t) % (2 * w)
+if x > w:
+    x = 2 * w - x
+y = (q + t) % (2 * h)
+if y > h:
+    y = 2 * h - y
+
+print(x, y)
