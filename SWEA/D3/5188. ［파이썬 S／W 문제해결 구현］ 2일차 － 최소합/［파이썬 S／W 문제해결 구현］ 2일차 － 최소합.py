b5ec8e49e69dad2f4f4113b4dbeb250a9c731aec
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-09 16:02:43 +0900

    [D3] Title: [파이썬 S/W 문제해결 구현] 2일차 - 최소합, Time: 1,831 ms, Memory: 89,088 KB -BaekjoonHub

--- SWEA/D3/5188. ［파이썬 S／W 문제해결 구현］ 2일차 － 최소합/［파이썬 S／W 문제해결 구현］ 2일차 － 최소합.py
+++ SWEA/D3/5188. ［파이썬 S／W 문제해결 구현］ 2일차 － 최소합/［파이썬 S／W 문제해결 구현］ 2일차 － 최소합.py
@@ -1,24 +1,23 @@
+def solve(r, c, cost):
+    global min_cost
+    if r >= N or c >= N:  # 비정상 범위
+        return
+    if (r, c) == (N - 1, N - 1):  # 우측하단, 목적지 도착
+        # 비용계산
+        total = cost + data[r][c]
+        if total < min_cost:
+            min_cost = total
+        return
+    # 현재 위치 r,c에서 내가 할 수 있는 일?
+    # 오른쪽 이동, 아래쪽 이동
+    solve(r, c + 1, cost + data[r][c])
+    solve(r + 1, c, cost + data[r][c])
+
+
 T = int(input())
-
-
-def backtracking(x, y, num):
-    global min_cnt
-    if x >= N or y >= N:
-        return
-
-    elif x == N - 1 and y == N - 1:
-        if num + table[x][y] < min_cnt:
-            min_cnt = num + table[x][y]
-        return
-
-    else:
-        backtracking(x + 1, y, num + table[x][y])
-        backtracking(x, y + 1, num + table[x][y])
-
-
 for tc in range(1, T + 1):
     N = int(input())
-    table = [list(map(int, input().split())) for _ in range(N)]
-    min_cnt = 1000
-    backtracking(0, 0, 0)
-    print(f"#{tc} {min_cnt}")
+    data = [list(map(int, input().split())) for _ in range(N)]
+    min_cost = 1000
+    solve(0, 0, 0)
+    print(f'#{tc} {min_cost}')
