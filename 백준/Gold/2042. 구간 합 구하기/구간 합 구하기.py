e5788eb7df7ade533b958b715d1648eaf4440791
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-13 11:45:24 +0900

    [Gold I] Title: 구간 합 구하기, Time: 3616 ms, Memory: 79784 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -0,0 +1,40 @@
+import sys
+input = sys.stdin.readline
+
+def update(idx, val):
+    bucket[idx//size] += val - data[idx]
+    data[idx] = val
+
+def query(left, right):
+    cnt = 0
+    while left <= right and left % size != 0:
+        cnt += data[left]
+        left += 1
+
+    while left + size <= right:
+        cnt += bucket[left // size]
+        left += size
+
+    while left <= right:
+        cnt += data[left]
+        left += 1
+
+    return cnt
+
+
+N, M, K = map(int, input().split())
+size = int(N ** 0.5)
+data = [0] * N
+for i in range(N):
+    data[i] = int(input().strip())
+
+bucket = [0] * N
+for i in range(N):
+    bucket[i // size] += data[i]
+
+for _ in range(M + K):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        update(b - 1, c)
+    elif a == 2:
+        print(query(b - 1, c - 1))
