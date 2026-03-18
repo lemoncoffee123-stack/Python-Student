d1d57e63cad90b55c576ec0cf68d2c7adff7550f
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-18 17:02:56 +0900

    [Gold IV] Title: 사이클 게임, Time: 668 ms, Memory: 52864 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -2,30 +2,28 @@ import sys
 sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-
-def union(x, y):
-    X, Y = find(x), find(y)
-    if X != Y:
-        uf[X] = Y
-
-
-def find(x):
-    if uf[x] == x:
-        return x
-
-    uf[x] = find(uf[x])
-    return uf[x]
-
-
 n, m = map(int, input().split())
 uf = [i for i in range(n)]
 for i in range(1, m+1):
     a, b = map(int, input().split())
 
-    if find(a) == find(b):
+    curr_a, curr_b = a, b
+
+    while uf[curr_a] != curr_a:
+        uf[curr_a] = uf[uf[curr_a]]
+        curr_a = uf[curr_a]
+    root_a = curr_a
+
+    while uf[curr_b] != curr_b:
+        uf[curr_b] = uf[uf[curr_b]]
+        curr_b = uf[curr_b]
+    root_b = curr_b
+
+    if root_a == root_b:
         print(i)
         exit()
 
-    union(a, b)
+    uf[root_a] = root_b
+
 else:
     print(0)
