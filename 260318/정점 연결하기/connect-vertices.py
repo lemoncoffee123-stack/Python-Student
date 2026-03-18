e2b7ea866dbfa993ccaad545384aadf6a9ae4983
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-18 15:08:17 +0900

    Title: 정점 연결하기, Time: 199ms, Memory: 25MB, Status: Wrong Answer - Codetree

---
+++ 260318/정점 연결하기/connect-vertices.py
@@ -0,0 +1,27 @@
+def union(x, y):
+    X, Y = find(x), find(y)
+    uf[Y] = X
+
+
+def find(x):
+    if uf[x] == x:
+        return x
+
+    root_node = find(uf[x])
+    uf[x] = root_node
+    return root_node
+
+
+n = int(input())
+uf = [i for i in range(n + 1)]
+result = set()
+while True:
+    try:
+        a, b = map(int, input().split())
+        union(a, b)
+    except:
+        break
+for i in range(1, n + 1):
+    result.add(find(i))
+
+print(*result)
