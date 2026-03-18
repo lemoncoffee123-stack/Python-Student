ba29bbb080327fb91af7b1499413a6c2e4b6ad1d
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-18 14:47:46 +0900

    Title: 연결된 정점, Time: 527ms, Memory: 25MB, Status: Passed - Codetree

--- 260318/연결된 정점/connected-vertex.py
+++ 260318/연결된 정점/connected-vertex.py
@@ -1,6 +1,8 @@
 def union(x, y):
     X, Y = find(x), find(y)
-    uf[X] = Y
+    if X != Y:
+        uf[X] = Y
+        size[Y] += size[X]
     return
 
 
@@ -14,14 +16,11 @@ def find(x):
 
 n, m = map(int, input().split())
 uf = [i for i in range(n + 1)]
+size = [1] * (n + 1)
 for _ in range(m):
     data = list(input().split())
     if data[0] == 'x':
         union(int(data[1]), int(data[2]))
     else:
-        cnt = 0
-        for i in range(1, n + 1):
-            if find(int(data[1])) == find(i):
-                cnt += 1
-
-        print(cnt)
+        root = find(int(data[1]))
+        print(size[root])
