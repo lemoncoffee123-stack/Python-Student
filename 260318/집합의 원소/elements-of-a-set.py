f792f861b0b09f2122fb3c5584e0a028ed32a326
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-18 10:05:45 +0900

    Title: 집합의 원소, Time: 448ms, Memory: 25MB, Status: Wrong Answer - Codetree

---
+++ 260318/집합의 원소/elements-of-a-set.py
@@ -0,0 +1,25 @@
+def union(x, y):
+    X, Y = find(x), find(y)
+    uf[X] = Y
+    return
+
+def find(x):
+    if uf[x] == x:
+        return x
+    root_node = find(uf[x])
+    uf[x] = root_node
+    return root_node
+
+
+
+N, M = map(int, input().split())
+uf = [i for i in range(N + 1)]
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    if a == 0:
+        union(b, c)
+    else:
+        if uf[b] == uf[c]:
+            print(1)
+        else:
+            print(0)
