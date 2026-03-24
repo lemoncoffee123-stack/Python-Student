196b00038af57bba45d4775eb991e21427309802
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-24 14:07:42 +0900

    Title: 팰린드롬과 쿼리, Time: 6052ms, Memory: 35MB, Status: Time Limit Exceed - Codetree

---
+++ 260324/팰린드롬과 쿼리/palindrome-and-query.py
@@ -0,0 +1,28 @@
+n, q = map(int, input().split())
+word = input()
+input_str = '#' + '#'.join(word) + '#'
+word_len = len(input_str)
+A = [0] * word_len
+p = r = -1
+for i in range(word_len):
+    if r < i:
+        A[i] = 0
+    
+    else:
+        ii = 2 * p - i
+        A[i] = min(r - i, A[ii])
+
+    while i - A[i] - 1 >= 0 and i + A[i] + 1 < word_len and input_str[i - A[i] - 1] == input_str[i + A[i] + 1]:
+        A[i] += 1
+
+    if i + A[i] > r:
+        r, p = i + A[i], i
+
+for _ in range(q):
+    a, b = map(int, input().split())
+    i, j = a * 2 - 1, b * 2 - 1
+    S = A[i:j+1]
+    if S[::] == S[::-1]:
+        print("Yes")
+    else:
+        print("No")
