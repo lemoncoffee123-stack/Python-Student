5bfbea61e10e44de390de9ed4feaa11f35de7ffb
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-26 23:30:21 +0900

    Title: 부분 문자열의 위치 구하기, Time: 0ms, Memory: 0MB, Status: Time Limit Exceed - Codetree

--- 260326/부분 문자열의 위치 구하기/find-the-location-of-a-substring.py
+++ 260326/부분 문자열의 위치 구하기/find-the-location-of-a-substring.py
@@ -1,38 +1,71 @@
-import sys
-input = sys.stdin.readline
+# 변수 선언 및 입력:
+text = input()
+pattern = input()
 
-N = input().strip()
-L = input().strip()
-n = len(N)
-l = len(L)
+n = len(text)
+l = len(pattern)
 
-p = 31
-mod = 10**9 + 7
+# 2개의 polynomial rolling 해싱을 위한 p, m 값을 정의합니다.
+p = [31, 37]
+m = [int(1e9) + 7, int(1e9) + 9]
 
-p_mod = [0] * (n  + 1)
-p_mod[0] = 1
+# p^i, 값을 m으로 나눈 나머지를 관리합니다.
+p_pow = [
+    [0] * (n + 1)
+    for _ in range(2)
+]
 
-N_arr = [ord(c) - 96 for c in N]
-L_arr = [ord(c) - 96 for c in L]
 
-for i in range(1, n + 1):
-    p_mod[i] = (p_mod[i - 1] * p) % mod
+# 소문자 알파벳을 수로 변경합니다.
+def to_int(c):
+    return ord(c) - ord('a') + 1
 
-l_h, n_h = 0, 0
-for i in range(l):
-    l_h = (l_h * p + L_arr[i]) % mod
-    n_h = (n_h * p + N_arr[i]) % mod
 
-if l_h == n_h:
+# p_pow 값을 계산합니다.
+# p_pow[i] = p^i % m
+for k in range(2):
+    # p_pow[i] = p^i % m
+    p_pow[k][0] = 1
+    for i in range(1, n + 1):
+        p_pow[k][i] = (p_pow[k][i - 1] * p[k]) % m[k]
+
+# pattern에 대한 해싱값인 p_h값을 계산합니다.
+# p_h = (pattern[0] * p^(l - 1) + pattern[1] * p^(l - 2) + ... + pattern[l - 1] * 1) % m
+# 소문자 알파벳은 a부터 z까지 순서대로 1부터 26까지의 수와 대응됩니다.
+p_h = [0, 0]
+for k in range(2):
+    for i in range(l):
+        p_h[k] = (p_h[k] + to_int(pattern[i]) * p_pow[k][l - 1 - i]) % m[k]
+
+# text에서 구간 [0, l - 1]에 해당하는 해싱값을 계산합니다.
+t_h = [0, 0]
+for k in range(2):
+    for i in range(l):
+        t_h[k] = (t_h[k] + to_int(text[i]) * p_pow[k][l - 1 - i]) % m[k]
+
+# 이미 일치한다면 답은 0이 됩니다.
+if p_h[0] == t_h[0] and p_h[1] == t_h[1]:
     print(0)
-    exit()
+    quit()
 
-p_l = p_mod[l]
+# text에서
+# 길이가 l인 부분문자열을 전부 잡아보며
+# 해싱값이 일치하는 위치를 찾습니다.
+ans = -1
 for i in range(1, n - l + 1):
-    n_h = (n_h * p - N_arr[i - 1] * p_l + N_arr[i + l - 1]) % mod
+    for k in range(2):
+        # 이전 [i - 1, i + l - 2]에 해당하는 해싱값은 t_h에 있습니다.
+        # 이전 값(t_h)은 (text[i - 1] * p^(l - 1) + text[i] * p^(l - 2) + ... + text[i + l - 2] * 1) % m입니다.
+        # 이제 t_h * p - text[i - 1] * p^l + text[i + l - 1]를 계산하면
+        # 새로 계산을 원하는 해싱값인 (text[i] * p^(l - 1) + text[i + 1] * p(l - 2) + ... + text[i + l - 1] * 1) % m이 됩니다.
+        t_h[k] = (t_h[k] * p[k] - to_int(text[i - 1]) * p_pow[k][l] + to_int(text[i + l - 1])) % m[k]
+        # t_h값을 양수로 변환해줍니다.
+        if t_h[k] < 0:
+            t_h[k] += m[k]
 
-    if n_h == l_h:
-        print(i)
-        exit()
+    # 값이 일치한다면 답이 됩니다.
+    if p_h[0] == t_h[0] and p_h[1] == t_h[1]:
+        ans = i
+        break
 
-print(-1)
+print(ans)
