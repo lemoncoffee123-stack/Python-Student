2ba3287707e6a2243c4a6f67df091647b03d42aa
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-26 23:02:39 +0900

    Title: 부분 문자열의 위치 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260326/부분 문자열의 위치 구하기/find-the-location-of-a-substring.py
+++ 260326/부분 문자열의 위치 구하기/find-the-location-of-a-substring.py
@@ -1,5 +1,8 @@
-N = input()
-L = input()
+import sys
+input = sys.stdin.readline
+
+N = input().strip()
+L = input().strip()
 n = len(N)
 l = len(L)
 
@@ -9,17 +12,16 @@ mod = 10**9 + 7
 p_mod = [0] * (n  + 1)
 p_mod[0] = 1
 
-def to_int(num):
-    return ord(num) - ord('a') + 1
-
+N_arr = [ord(c) - 96 for c in N]
+L_arr = [ord(c) - 96 for c in L]
 
 for i in range(1, n + 1):
     p_mod[i] = (p_mod[i - 1] * p) % mod
 
 l_h, n_h = 0, 0
 for i in range(l):
-    l_h = (l_h * p + to_int(L[i])) % mod
-    n_h = (n_h * p + to_int(N[i])) % mod
+    l_h = (l_h * p + L_arr[i]) % mod
+    n_h = (n_h * p + N_arr[i]) % mod
 
 if l_h == n_h:
     print(0)
@@ -27,7 +29,7 @@ if l_h == n_h:
 
 p_l = p_mod[l]
 for i in range(1, n - l + 1):
-    n_h = (n_h * p - to_int(N[i - 1]) * p_l + to_int(N[i + l - 1])) % mod
+    n_h = (n_h * p - N_arr[i - 1] * p_l + N_arr[i + l - 1]) % mod
 
     if n_h == l_h:
         print(i)
