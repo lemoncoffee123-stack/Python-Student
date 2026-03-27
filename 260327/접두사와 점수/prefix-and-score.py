6ac4f9a3dbc0ce136f1d8f74d5d0f92d2854ba36
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-27 16:52:14 +0900

    Title: 접두사와 점수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260327/접두사와 점수/prefix-and-score.py
@@ -0,0 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.count += 1
+
+        node.is_end = True
+
+    
+    def search(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return 0
+            node = node.children[char]
+        return node.count * len(word)
+
+
+trie = Trie()
+N = int(input())
+words = list(input().split())
+for word in words:
+    trie.insert(word)
+
+result = 0
+for word in words:
+    result = max(result, trie.search(word))
+
+print(result)
+    
