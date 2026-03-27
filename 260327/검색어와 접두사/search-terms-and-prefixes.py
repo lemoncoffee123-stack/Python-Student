df143943c2a1183314efd6d4186cd7e23d123afb
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-03-27 16:27:34 +0900

    Title: 검색어와 접두사, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260327/검색어와 접두사/search-terms-and-prefixes.py
@@ -0,0 +1,45 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+        self.count = 0
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+            node.count += 1
+
+        node.is_end = True
+
+    
+    def search(self, prefix, len_prefix):
+        node = self.root
+        for i in range(len_prefix):
+            if prefix[i] not in node.children:
+                print(node.count, end=" ")
+                for _ in range(len_prefix - i):
+                    print(0, end=" ")
+                return 
+
+            if i != 0:
+                print(node.count, end=" ")
+            node = node.children[prefix[i]]
+
+
+trie = Trie()
+n, m = map(int, input().split())
+words = list(input().split())
+for word in words:
+    trie.insert(word)
+    
+find_word = input()
+trie.search(find_word, m)
