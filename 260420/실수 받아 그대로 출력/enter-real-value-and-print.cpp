9f3d09de6396a8f2d459f50008b580786f1554cc
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-20 14:20:59 +0900

    Title: 실수 받아 그대로 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double a;
+    cin >> a;
+    cout.fixed;
+    cout << fixed;
+    cout.precision(2);
+    cout << a;
+    return 0;
+}
