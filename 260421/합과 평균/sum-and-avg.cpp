959ac0db5a27c90e8b994c32ca78eaa144bdd555
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-21 16:04:47 +0900

    Title: 합과 평균, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a, b;
+    cin >> a >> b;
+    int total = a + b;
+    double average = (double)total / 2;
+    cout << total << " " << average;
+    return 0;
+}
