9fae60c8a49a6104e93d96c48aef8a3541372840
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-21 17:51:25 +0900

    Title: 물의 상태, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260421/물의 상태/state-of-water.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    if (n >= 100) cout << "vapor";
+    else if (n >= 0) cout << "water";
+    else cout << "ice";
+    return 0;
+}
