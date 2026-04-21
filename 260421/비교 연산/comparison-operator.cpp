894993553e61c76872a0d3ca8d00b637fdbbb56e
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-21 16:32:30 +0900

    Title: 비교 연산, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260421/비교 연산/comparison-operator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int A, B;
+    cin >> A >> B;
+    if (A >= B) cout << 1 << endl;
+    else cout << 0 << endl;
+    if (A > B) cout << 1 << endl;
+    else cout << 0 << endl;
+    if (B >= A) cout << 1 << endl;
+    else cout << 0 << endl;
+    if (B > A) cout << 1 << endl;
+    else cout << 0 << endl;
+    if (A == B) cout << 1 << endl;
+    else cout << 0 << endl;
+    if (A != B) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    return 0;
+}
