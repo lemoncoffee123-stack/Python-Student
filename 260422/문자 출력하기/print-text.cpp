212b08f4b5ae5752cfa54aa314a24771f0c7fae7
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-22 09:33:02 +0900

    Title: 문자 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/문자 출력하기/print-text.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char a;
+    int i;
+    cin >> a;
+    for(i = 0; i < 8; i++) cout << a;
+    return 0;
+}
