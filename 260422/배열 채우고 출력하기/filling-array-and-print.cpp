13e0347abdbb59d2f945e9deacf4208ed5fced05
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-22 10:43:24 +0900

    Title: 배열 채우고 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char array[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> array[i];
+    }
+ 
+    for (int i = 9; i > -1; i -= 1) {
+        cout << array[i];
+    }
+    return 0;
+}
