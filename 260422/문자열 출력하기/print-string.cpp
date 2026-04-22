07ef4472d84745499dd92845f0b6502738cbb908
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-22 09:35:03 +0900

    Title: 문자열 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/문자열 출력하기/print-string.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, i;
+    cin >> n;
+    for (i = 0; i < n; i++) cout << "LeebrosCode" << endl;
+    return 0;
+}
