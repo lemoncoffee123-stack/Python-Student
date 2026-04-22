e70e79a1d938c6791488fc2552b2e0f7863b1a0c
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-04-22 17:00:02 +0900

    Title: 0 나올때까지 출력, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260422/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while (true) {
+        int a;
+        cin >> a;
+        if (a == 0) {
+            break;
+        }
+        else cout << a << endl;
+    }
+    return 0;
+}
