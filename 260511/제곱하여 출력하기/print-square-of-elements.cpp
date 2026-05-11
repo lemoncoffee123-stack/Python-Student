8766ea3cb9add30f5b3fa8a7dd0d079a3cbbe819
lemoncoffee123-stack <lemoncoffee123@gmail.com>
2026-05-11 13:39:33 +0900

    [Passed] 제곱하여 출력하기 (8ms, 0MB)

---
+++ 260511/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, val;
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> val;
+        cout << val * val << " ";
+    }
+    return 0;
+}
